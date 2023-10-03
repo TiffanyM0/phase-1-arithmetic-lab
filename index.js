9410951741aac1dca3802b773f331d6c1def9628
TIFFANY MAINA <tiffmaina@gmail.com>
2023-10-03 09:08:05 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+// Write your code here'
+let num1 = 31;
+let num2 = 2;
+const multiply = (num1 * num2)
+// function multiply(num1, num2){
+//     return (31*2);
+// }
 
-// Write your code here
+const random = Math.round(Math.random())
+
+let num3 = 34;
+let num4 = 5;
+const mod = (num3%num4);
+
+const max = (Math.max(1,3,5,7,20));
+
+
+
+
